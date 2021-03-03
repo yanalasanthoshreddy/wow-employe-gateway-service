40964c8314c55a13ae12d9c4a1d3bc544e376381
yanalasanthoshreddy <santhosh.201316@gmail.com>
2021-03-03 21:38:41 +0530

    circuit breaker code is working

--- src/main/java/com/emp/application/FallBackController.java
+++ src/main/java/com/emp/application/FallBackController.java
@@ -3,9 +3,6 @@ package com.emp.application;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -13,12 +10,12 @@ import lombok.extern.slf4j.Slf4j;
 public class FallBackController {
 
 	@GetMapping("/employeeFallBack")
-	public String employeeFallBack() {
+	public String employeeServiceFallBack() {
 		log.info("Employee Service is taking long time respond, Please try again Later");
 		return "Employee Service is taking long time respond, Please try again Later";
 	}
 	@GetMapping("/pharmacyFallBack")
-	public String pharmacyFallBack() {
+	public String pharmacyServiceFallBack() {
 		log.info("Pharmacy Service is taking long time respond, Please try again Later");
 		return "Pharmacy Service is taking long time respond, Please try again Later";
 	}
